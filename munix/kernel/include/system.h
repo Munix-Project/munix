6d7db83064a3780c6794b1aca36af17ace1cc289
miguelangelo78 <santosmiguel25@gmail.com>
2015-08-13 03:19:12 +0100

    Boot code added

---
+++ munix/kernel/include/system.h
@@ -0,0 +1,15 @@
+/*
+ * system.h
+ *
+ *  Created on: Aug 13, 2015
+ *      Author: miguel
+ */
+
+#ifndef KERNEL_INCLUDE_SYSTEM_H_
+#define KERNEL_INCLUDE_SYSTEM_H_
+
+#include <types.h>
+
+
+
+#endif /* KERNEL_INCLUDE_SYSTEM_H_ */
---
+++ munix/kernel/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 13, 2015
+ *      Author: miguel
+ */
+#include <multiboot.h>
+
+int main(struct multiboot *mboot, uint32_t mboot_mag, uintptr_t esp){
+
+	return 0;
+}
+
+
