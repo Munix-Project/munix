7a15cb30e1bd563af9359598afef237134f6dbec
miguelangelo78 <santosmiguel25@gmail.com>
2015-09-11 10:41:06 +0100

    generate_sym.sh script altered

--- munix_toolchain/util/symbols/generate_sym.sh
+++ munix_toolchain/util/symbols/generate_sym.sh
@@ -7,7 +7,7 @@ TOOLCHAIN=munix_toolchain
 KERNELNAME=munix-kernel
 PYPATH=./$TOOLCHAIN/util/symbols/generate_symbols.py
 SYMBOLASM=./kernel/symbols.S
-NM=i686-pc-toaru-nm
+NM=i686-munix-nm
 
 # Now generate the symbols:
 $NM ./$TOOLCHAIN/$KERNELNAME -g | $PYPATH > $SYMBOLASM
